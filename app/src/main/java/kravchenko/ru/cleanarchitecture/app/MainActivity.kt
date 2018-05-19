4936f42006bd40a7f1fde7eb7c2cbefbe36ece6c
Andrey Kravchenko <forletters01@gmail.com>
2018-05-19 14:00:23 +0300

    f5.1

--- app/src/main/java/kravchenko/ru/cleanarchitecture/app/MainActivity.kt
+++ app/src/main/java/kravchenko/ru/cleanarchitecture/app/MainActivity.kt
@@ -19,5 +19,6 @@ class MainActivity : AppCompatActivity() {
         //commit for some feature_two
         //commit for some f3
         //commit for some f5
+        //commit for some f5.1
     }
 }
