d091ec4818750c8724d06e998a64bf8535320d70
Andrey Kravchenko <forletters01@gmail.com>
2018-05-19 13:55:57 +0300

    f4

--- app/src/main/java/kravchenko/ru/cleanarchitecture/app/MainActivity.kt
+++ app/src/main/java/kravchenko/ru/cleanarchitecture/app/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val presenter = MainActivityPresenter()
+        //commit for some f4
 
         presenter.getMedOrgs()
 
